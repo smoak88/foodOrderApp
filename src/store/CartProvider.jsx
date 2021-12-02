a5596efcfd2e0dd42b329f43b19f982867680b7a
smoak88 <smoak88@gmail.com>
2021-12-02 11:49:38 -0800

    Add useReducer for managing the cart

--- src/store/CartProvider.jsx
+++ src/store/CartProvider.jsx
@@ -1,19 +1,49 @@
 import CartContext from "./cart-context";
+import { useReducer } from "react";
+
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
+
+const cartReducer = (state, action) => {
+  if (action.type === "ADD") {
+    const updatedItems = state.items.concat(action.item);
+    const updatedTotalAmount =
+      state.totalAmount + action.item.price * action.item.amount;
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
+  }
+  return defaultCartState;
+};
+
+/* ^^^^^Concat adds an item to array and returns a new array, versus push which adds an item to the array and then returns the original array. Concat is better, dont want to deal with editing old state snapshot in memory. */
 
 const CartProvider = (props) => {
-  const addItemToCartHandler = (props) => {};
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
 
-  const removeItemFromCartHandler = (props) => {};
+  const addItemToCartHandler = (item) => {
+    dispatchCartAction({ type: "ADD", item: item });
+  };
 
-  const CartContext = {
-    items: [],
-    totalAmount: 0,
+  const removeItemFromCartHandler = (id) => {
+    dispatchCartAction({ type: "REMOVE", id: id });
+  };
+
+  const cartContext = {
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
   };
 
   return (
-    <CartContext.Provider value={CartContext}>
+    <CartContext.Provider value={cartContext}>
       {props.children}
     </CartContext.Provider>
   );
